e80084b134b30cc9c057acae934ddfc29a8ed500
William MR <williammr@Williams-MacBook-Pro.local>
2019-12-25 19:24:21 -0500

    Removing unused security groups

--- security_groups.tf
+++ security_groups.tf
@@ -101,3 +101,79 @@ module "db_sg" {
         Name = "DBServerSG"
     }
 }
+
+/* NAT Security Group */
+
+# module "nat_sg" {
+#   source = "terraform-aws-modules/security-group/aws"
+
+#   name        = "nat_sg"
+#   description = "Allow trafic to pass from the private subnet to the internet"
+#   vpc_id      = aws_vpc.default.id
+
+#   ingress_with_cidr_blocks = [
+#     {
+#       from_port   = 80
+#       to_port     = 80
+#       protocol    = "tcp"
+#       description = "HTTP Ports"
+#       cidr_blocks = var.private_subnet_cidr
+#     },
+#     {
+#       from_port   = 443
+#       to_port     = 443
+#       protocol    = "tcp"
+#       description = "HTTPS ports"
+#       cidr_blocks = var.private_subnet_cidr
+#     },
+#     {
+#       from_port   = 22
+#       to_port     = 22
+#       protocol    = "tcp"
+#       description = "SSH ports"
+#       cidr_blocks = "0.0.0.0/0"
+#     },
+#     {
+#       from_port   = -1
+#       to_port     = -1
+#       protocol    = "icmp"
+#       description = "Allow ICMP"
+#       cidr_blocks = "0.0.0.0/0"
+#     },
+#   ]
+
+#   egress_with_cidr_blocks = [
+#     {
+#       from_port   = 80
+#       to_port     = 80
+#       protocol    = "tcp"
+#       description = "HTTP Ports"
+#       cidr_blocks = "0.0.0.0/0"
+#     },
+#     {
+#       from_port   = 443
+#       to_port     = 443
+#       protocol    = "tcp"
+#       description = "HTTPS Ports"
+#       cidr_blocks = "0.0.0.0/0"
+#     },
+#     {
+#       from_port   = 22
+#       to_port     = 22
+#       protocol    = "tcp"
+#       description = "SSH Ports"
+#       cidr_blocks = "0.0.0.0/0"
+#     },
+#     {
+#       from_port   = 0
+#       to_port     = 0
+#       protocol    = "-1"
+#       description = "Allow to go anywhere"
+#       cidr_blocks = "0.0.0.0/0"
+#     },
+#   ]
+
+#   tags = {
+#         Name = "NAT SG"
+#     }
+# }
