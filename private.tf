e968ceb6a83d7485667df2093fd8236a5403c744
William MR <william.munozr@gmail.com>
2020-03-18 11:30:30 -0500

    Deny database creation different than db.t2.micro
    AWS must detect db.t2.small and deny the database creation

--- private.tf
+++ private.tf
@@ -16,7 +16,7 @@ resource "aws_db_instance" "default" {
     storage_type            = "gp2"
     engine                  = "mysql"
     engine_version          = "5.7"
-    instance_class          = "db.t2.micro"
+    instance_class          = "db.t2.small"
     name                    = "catsndogs"
     username                = "admindb"
     password                = var.database_password
