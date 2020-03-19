410f2b8c13d1277cf46487992a5ee2b22aa84926
William MR <william.munozr@gmail.com>
2020-03-19 16:25:12 -0500

    Create the infrastructure

--- state_config.tf
+++ state_config.tf
@@ -1,19 +1,19 @@
-# terraform { 
-#     backend "s3" {
-#         bucket          = "terraform-workshop-circleci"
-#         key             = "terraform-circleci-00/state"
-#         region          = "us-east-1"
-#         dynamodb_table  = "terraform-circleci-state"
-#         encrypt         = true
-#     }
-# }
-
 terraform { 
     backend "s3" {
-        bucket          = "tfstate-hachiko-app"
+        bucket          = "terraform-workshop-circleci"
         key             = "terraform-circleci-00/state"
         region          = "us-east-1"
-        dynamodb_table  = "terraform-state"
+        dynamodb_table  = "terraform-circleci-state"
         encrypt         = true
     }
 }
+
+#terraform { 
+#    backend "s3" {
+#        bucket          = "tfstate-hachiko-app"
+#        key             = "terraform-circleci-00/state"
+#        region          = "us-east-1"
+#        dynamodb_table  = "terraform-state"
+#        encrypt         = true
+#    }
+#}
