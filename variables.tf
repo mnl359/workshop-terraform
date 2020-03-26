bc632c31f94a88aa034e2191890c0a3bbb9b5bbc
William Munoz Rodas <williammr@Williams-iMac.local>
2020-03-26 15:48:21 -0500

    Limit number of subnets

--- variables.tf
+++ variables.tf
@@ -12,6 +12,10 @@ locals {
   cidr_c_private_subnets    = 1
   cidr_c_database_subnets   = 11
   cidr_c_public_subnets     = 64
+
+  max_private_subnets       = 2
+  max_database_subnets      = 2
+  max_public_subnets        = 2
 }
 
 locals {
@@ -30,16 +34,19 @@ locals {
     private_subnets = [
         for az in local.availability_zones :
             "${lookup(var.cidr_ab, var.environment)}.${local.cidr_c_private_subnets + index(local.availability_zones, az)}.0/24"
+            if index(local.availability_zones, az) < local.max_private_subnets
     ]
 
     database_subnets = [
         for az in local.availability_zones :
             "${lookup(var.cidr_ab, var.environment)}.${local.cidr_c_database_subnets + index(local.availability_zones, az)}.0/24"
+            if index(local.availability_zones, az) < local.max_database_subnets
     ]
 
     public_subnets = [
         for az in local.availability_zones :
             "${lookup(var.cidr_ab, var.environment)}.${local.cidr_c_public_subnets + index(local.availability_zones, az)}.0/24"
+            if index(local.availability_zones, az) < local.max_public_subnets
     ]
 }
 
