6fa3f9dd3d1238f0ecdad9014f0ce77805d6a076
William Munoz Rodas <williammr@Williams-iMac.local>
2020-03-26 15:13:27 -0500

    Change Terraform state to Personal AWS Account

--- state_config.tf
+++ state_config.tf
@@ -1,19 +1,19 @@
-terraform { 
-    backend "s3" {
-        bucket          = "a-circleci-terraform-state"
-        key             = "terraform-circleci-00/state"
-        region          = "us-east-1"
-        dynamodb_table  = "circleci-terraform-state"
-        encrypt         = true
-    }
-}
-
 #terraform { 
 #    backend "s3" {
-#        bucket          = "tfstate-hachiko-app"
+#        bucket          = "a-circleci-terraform-state"
 #        key             = "terraform-circleci-00/state"
 #        region          = "us-east-1"
-#        dynamodb_table  = "terraform-state"
+#        dynamodb_table  = "circleci-terraform-state"
 #        encrypt         = true
 #    }
 #}
+
+terraform { 
+    backend "s3" {
+        bucket          = "tfstate-hachiko-app"
+        key             = "terraform-circleci-00/state"
+        region          = "us-east-1"
+        dynamodb_table  = "terraform-state"
+        encrypt         = true
+    }
+}
